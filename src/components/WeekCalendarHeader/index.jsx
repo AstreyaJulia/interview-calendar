7a929cdc64079ac7b93988e09916f069c932b8fc
Julia Latysheva <astreyajulia@gmail.com>
2023-04-14 09:05:16 +0300

    Month select

--- src/components/WeekCalendarHeader/index.jsx
+++ src/components/WeekCalendarHeader/index.jsx
@@ -10,7 +10,6 @@ const WeekCalendarHeader = ({data}) => {
       border-bottom: 2px solid #ebebeb;
       display: flex;
       flex-direction: column;
-      gap: 10px;
     `
 
     const WeekDaysContainer = styled.div`
@@ -19,12 +18,51 @@ const WeekCalendarHeader = ({data}) => {
       grid-template-columns: repeat(8, 1fr);
     `
 
+    const MonthSelectContainer = styled.div`
+      width: 100%;
+      display: grid;
+      grid-template-columns: 1fr 7fr;
+    `
+
+    const MonthSelect = styled.div`
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 40px;
+    `
+    const MonthSelectButton = styled.button`
+      border: none;
+      background: none;
+      color: #ff3131;
+      font-size: 28px;
+      padding: 0;
+    `
+
+    const MonthSelectTitle = styled.h2`
+      color: #000000;
+      font-weight: 600;
+      padding: 0;
+      margin: 0;
+      font-size: 16px;
+      line-height: 1;
+    `
+
     return <WeekCalendarHeaderContainer>
         <WeekDaysContainer>
             <div/>
             {/* Пустая ячейка */}
             {data.map((day, key) => <DayItem key={key} day={day}/>)}
         </WeekDaysContainer>
+
+        <MonthSelectContainer>
+            <div/>
+            {/* Пустая ячейка */}
+            <MonthSelect>
+                <MonthSelectButton>{'<'}</MonthSelectButton>
+                <MonthSelectTitle>March 2019</MonthSelectTitle>
+                <MonthSelectButton>{'>'}</MonthSelectButton>
+            </MonthSelect>
+        </MonthSelectContainer>
 
     </WeekCalendarHeaderContainer>;
 };
